b882972baab0e40f238d52c35ec409c0597ef855
sistemasintegrale <side4367@gmail.com>
2023-07-04 09:19:10 -0500

    d

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,7 @@
 //TODO: Cambiar los URl
 export const environment = {
-  //base_url: 'https://www.novaglassbackend.somee.com/api',
-  base_url: 'https://localhost:7203/api',
+  base_url: 'https://www.novaglassbackend.somee.com/api',
+  //base_url: 'https://localhost:7203/api',
 
   CONN_NOVAGLASS: 1,
   CONN_NOVAMOTOS: 2
